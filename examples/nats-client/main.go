fe1739eb1c0b8ed1027902cf41034d5efca9456e
Joe Gorse <Joseph.GorseIii@emc.com>
2016-08-25 09:42:09 -0400

    Added connection to Nats server. Added example Nats listener client

---
+++ examples/nats-client/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"runtime"
+
+	"github.com/nats-io/nats"
+)
+
+var serverAddress, channel string
+
+func init() {
+	flag.StringVar(&serverAddress, "server-address", "nats://localhost:4222", "Address of the Nats server to listen to (default: nats://localhost:4222)")
+	flag.StringVar(&channel, "channel", "Snap", "The server channel to listen to (default: Snap)")
+}
+
+func main() {
+	flag.Parse()
+
+	nc, err := nats.Connect(serverAddress)
+	if err != nil {
+		log.Printf("Could not connect to Nats server: %s\n", err)
+		return
+	}
+
+	log.Printf("Connected to Nats server at %s\n", serverAddress)
+	// Simple Async Subscriber
+	nc.Subscribe(channel, func(m *nats.Msg) {
+		log.Printf("Listening to channel %s\n", channel)
+		log.Printf("Received a message: %s\n", string(m.Data))
+	})
+
+	runtime.Goexit()
+}
